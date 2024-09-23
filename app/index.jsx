12422da8f595ea1933973e75696052eb52a88ce5
Gunish.Jain <Gunish.Jain@IND-DEL-MX-M206s-MacBook-Pro.local>
2024-09-23 11:27:09 +0530

    added nativewind css

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Market Xcel!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Hello Market Xcel!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: "blue"}}>
       Go To Profile</Link>
@@ -13,11 +13,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
