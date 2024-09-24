7e1167f64bf07ba9179945727d465e257022f7c5
gunishjain <gunish.jain@gmail.com>
2024-09-24 11:45:23 +0530

    Add Assets and Folder Structure

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Hello Market Xcel!</Text>
+      <Text className="text-3xl font-pblack">Hello Market Xcel!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: "blue"}}>
       Go To Profile</Link>
